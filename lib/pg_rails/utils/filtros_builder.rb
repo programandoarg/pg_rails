2efc6af411d6333cbc62814046e63d2b4608dcb4
Martín Rosso <mrosso10@gmail.com>
2020-02-22 22:29:30 -0300

    filtros de fecha por rango desde/hasta

--- lib/pg_rails/utils/filtros_builder.rb
+++ lib/pg_rails/utils/filtros_builder.rb
@@ -21,6 +21,12 @@ module PgRails
           query = query.where("#{campo}_id = ?", params[campo])
         elsif tipo(campo) == :string
           query = query.where("#{campo} ILIKE '%#{params[campo]}%'")
+        elsif tipo(campo) == :date
+          begin
+            fecha = Date.parse(params[campo])
+            query = query.where("#{quitar_sufijo(campo)} #{comparador(campo)} ?", fecha)
+          rescue ArgumentError
+          end
         end
       end
       query
@@ -32,9 +38,33 @@ module PgRails
       elsif @clase_modelo.reflect_on_all_associations.find {|a| a.name == campo }.present?
         :asociacion
       else
+        campo = quitar_sufijo(campo)
         columna = @clase_modelo.columns.find {|columna| columna.name == campo.to_s}
+        if columna.nil?
+          Logueador.warning("no existe el campo: #{campo}")
+          return
+        end
         columna.type
       end
+    end
+
+    def comparador(campo)
+      if campo.to_s.ends_with?('_desde')
+        '>'
+      elsif campo.to_s.ends_with?('_hasta')
+        '<'
+      else
+        '='
+      end
+    end
+
+    def quitar_sufijo(campo)
+      ret = campo.to_s.dup
+      sufijos = ['_desde', '_hasta']
+      sufijos.each do |sufijo|
+        ret.gsub!(/#{sufijo}$/, '')
+      end
+      ret
     end
 
     def filtros_html
@@ -44,6 +74,8 @@ module PgRails
           res += filtro_select(campo, campo.to_s.humanize)
         elsif tipo(campo) == :asociacion
           res += filtro_asociacion(campo, campo.to_s.humanize)
+        elsif tipo(campo) == :date
+          res += filtro_fecha(campo, campo.to_s.humanize)
         else
           res += filtro_texto(campo, campo.to_s.humanize)
         end
@@ -76,6 +108,20 @@ module PgRails
         content_tag :div, class: 'input-group', style: 'width:230px' do
           text_field_tag(
             campo, params[campo], class: "form-control", placeholder: placeholder, autocomplete: "off"
+          ) + content_tag(:span, class: 'input-group-btn', type: :submit) do
+            button_tag class: 'btn btn-primary disabled' do
+              content_tag :span, nil, class: 'fa fa-search'
+            end
+          end
+        end
+      end
+    end
+
+    def filtro_fecha(campo, placeholder = '')
+      content_tag :div, class: 'filter' do
+        content_tag :div, class: 'input-group', style: 'width:230px' do
+          text_field_tag(
+            campo, params[campo], class: "form-control datefield", placeholder: placeholder, autocomplete: "off"
           ) + content_tag(:span, class: 'input-group-btn', type: :submit) do
             button_tag class: 'btn btn-primary disabled' do
               content_tag :span, nil, class: 'fa fa-search'
