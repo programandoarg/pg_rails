fd33522cbd404d35ee094315bb13ce46a394f438
Martín Rosso <mrosso10@gmail.com>
2020-02-23 21:27:30 -0300

    chosen-select

--- lib/pg_rails/simple_form/initializer.rb
+++ lib/pg_rails/simple_form/initializer.rb
@@ -145,6 +145,17 @@ SimpleForm.setup do |config|
     b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback d-block' }
     b.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
   end
+  # vertical multi select
+  config.wrappers :chosen_select, tag: 'div', class: 'form-group', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
+    b.use :html5
+    b.optional :readonly
+    b.use :label, class: 'form-control-label'
+    b.wrapper tag: 'div', class: 'd-flex flex-row justify-content-between align-items-center' do |ba|
+      ba.use :input, class: 'form-control chosen-select mx-1', error_class: 'is-invalid', valid_class: 'is-valid'
+    end
+    b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback d-block' }
+    b.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+  end
 
   config.wrappers :selects_dependientes, tag: 'div', class: 'form-group dependent_fields', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
     b.use :html5
@@ -457,6 +468,7 @@ SimpleForm.setup do |config|
     radio_buttons: :vertical_collection,
     range:         :vertical_range,
     time:          :vertical_multi_select,
+    select:        :chosen_select,
   })
 
   # enable custom form wrappers
