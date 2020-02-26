f7b93b5579c69644715a0bc6ec75084f07049097
Martín Rosso <mrosso10@gmail.com>
2020-02-26 00:04:37 -0300

    muevo los require dentro del initializer

--- lib/pg_rails/engine.rb
+++ lib/pg_rails/engine.rb
@@ -1,16 +1,15 @@
-require 'rails'
-require 'pg_rails/monkey_patches/mejoras_de_atributos'
-require 'pg_rails/monkey_patches/mejoras_a_named_base'
-
-require 'pg_rails/utils/filtros_builder'
-require 'pg_rails/utils/logueador'
-require 'pg_rails/simple_form/initializer'
-
 module PgRails
   class Engine < ::Rails::Engine
     isolate_namespace PgRails
 
     initializer 'configurar_generators', after: "factory_bot.set_fixture_replacement" do
+      require 'pg_rails/monkey_patches/mejoras_de_atributos'
+      require 'pg_rails/monkey_patches/mejoras_a_named_base'
+
+      require 'pg_rails/utils/filtros_builder'
+      require 'pg_rails/utils/logueador'
+      require 'pg_rails/simple_form/initializer'
+
       config.app_generators do |g|
         g.fixture_replacement :pg_factory_bot, dir: 'spec/factories'
         g.test_framework :pg_rspec
--- lib/pg_rails/version.rb
+++ lib/pg_rails/version.rb
@@ -1,3 +1,3 @@
 module PgRails
-  VERSION = '0.1.3'
+  VERSION = '0.1.4'
 end
