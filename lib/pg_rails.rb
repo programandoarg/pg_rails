ff4507dda1ce3524ba0a2ff575b428979424fe91
Martín Rosso <mrosso10@gmail.com>
2020-03-05 10:52:31 -0300

    configuracion de clases de iconos y botones

--- lib/pg_rails.rb
+++ lib/pg_rails.rb
@@ -1,5 +1,16 @@
 require "pg_rails/engine"
+require "pg_rails/configuracion"
 
 module PgRails
-  # Your code goes here...
+  class << self
+    attr_writer :configuracion
+
+    def configuracion
+      @configuracion ||= Configuracion.new
+    end
+
+    def configurar
+      yield(configuracion)
+    end
+  end
 end
