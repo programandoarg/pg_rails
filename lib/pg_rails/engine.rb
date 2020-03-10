4315a9380925ca506f7859fcce3be3a1567f5076
Martín Rosso <mrosso10@gmail.com>
2020-03-10 11:52:56 -0300

    app_generators (para que los tome la main_app)

--- lib/pg_rails/engine.rb
+++ lib/pg_rails/engine.rb
@@ -25,6 +25,15 @@ module PgRails
         g.fallbacks[:pg_rspec] = :rspec
         g.fallbacks[:pg_active_record] = :active_record
       end
+
+      config.app_generators do |g|
+        g.fixture_replacement :pg_factory_bot, dir: 'spec/factories'
+        g.test_framework :pg_rspec
+        g.orm :pg_active_record
+
+        g.fallbacks[:pg_rspec] = :rspec
+        g.fallbacks[:pg_active_record] = :active_record
+      end
     end
 
     if Rails.env.test?
