2f7632f91579d6a75429ef34b986f7513ef95f03
Martín Rosso <mrosso10@gmail.com>
2020-03-13 11:52:41 -0300

    rspec model generator

--- lib/generators/pg_rspec/model/model_generator.rb
+++ lib/generators/pg_rspec/model/model_generator.rb
@@ -1,19 +1,16 @@
-require 'generators/rspec/scaffold/scaffold_generator'
+require 'generators/rspec/model/model_generator'
 
 module PgRspec
   module Generators
-    class ScaffoldGenerator < Rspec::Generators::ScaffoldGenerator
+    class ModelGenerator < Rspec::Generators::ModelGenerator
       # agrego la carpeta para que busque templates
       # manteniendo la source_root de Rspec::Generators::ScaffoldGenerator
       # para poder copiar sólo los que quiero cambiar
       source_paths << File.expand_path('templates', __dir__)
 
-      remove_hook_for :integration_tool, :as => :integration
       remove_hook_for :fixture_replacement
 
-      def generate_routing_spec
-        # piso el método para que no genere nada
-      end
+      invoke 'pg_factory_bot:model'
     end
   end
 end
