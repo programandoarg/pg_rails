01b24cab34b6ac681cb9c18cc94fae842293b36f
Martín Rosso <mrosso10@gmail.com>
2020-03-14 16:13:08 -0300

    paranoia y trackeo de usuarios opcionales

--- lib/generators/pg_active_record/model/model_generator.rb
+++ lib/generators/pg_active_record/model/model_generator.rb
@@ -3,5 +3,10 @@ require "rails/generators/active_record/model/model_generator"
 module PgActiveRecord
   class ModelGenerator < ActiveRecord::Generators::ModelGenerator
     source_paths << File.expand_path('templates', __dir__)
+
+    class_option :paranoia, type: :boolean, default: false,
+            desc: "Paranoid y deleted_at."
+    class_option :trackeo_de_usuarios, type: :boolean, default: true,
+            desc: "Genera campos creado_por y actualizado_por."
   end
 end
--- lib/generators/pg_active_record/model/templates/model.rb
+++ lib/generators/pg_active_record/model/templates/model.rb
@@ -1,3 +1,4 @@
+
 # generado con pg_rails
 
 <% if namespaced? -%>
@@ -7,24 +8,35 @@ require_dependency "<%= namespaced_path %>/application_record"
 <% module_namespacing do -%>
 class <%= class_name %> < <%= parent_class_name.classify %>
   audited
+  <%- if options[:paranoia] -%>
+  acts_as_paranoid without_default_scope: true
+  <%- end -%>
+  <%- if attributes.any?(&:reference?) -%>
 
-<% attributes.select(&:reference?).each do |attribute| -%>
+    <%- attributes.select(&:reference?).each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', optional: true' unless attribute.required? %><%= ", class_name: '#{attribute.clase_con_modulo}'" if attribute.tiene_nombre_de_clase_explicito? %>
-<% end -%>
+    <%- end -%>
+  <%- end -%>
+  <%- if options[:trackeo_de_usuarios] -%>
+
   belongs_to :creado_por, optional: true, class_name: 'User'
   belongs_to :actualizado_por, optional: true, class_name: 'User'
-<% attributes.select(&:token?).each do |attribute| -%>
-  has_secure_token<% if attribute.name != "token" %> :<%= attribute.name %><% end %>
-<% end -%>
-<% if attributes.any?(&:password_digest?) -%>
-  has_secure_password
-<% end -%>
-<% if attributes.any? { |at| at.required? } -%>
-  validates <%= attributes.select { |at| at.required? }.map { |at| ":#{at.name}" }.join(', ') %>, presence: true
-<% end -%>
+  <%- end -%>
+  <%- if attributes.any?(&:es_enum?) -%>
 
-<% attributes.select(&:es_enum?).each do |attribute| -%>
+    <%- attributes.select(&:es_enum?).each do |attribute| -%>
   enumerize :<%= attribute.name %>, in: { completar: 0, los: 1, valores: 2}
-<% end -%>
+    <%- end -%>
+  <%- end -%>
+  <%- if attributes.any?(&:required?) -%>
+
+  validates <%= attributes.select(&:required?).map { |at| ":#{at.name}" }.join(', ') %>, presence: true
+  <%- end -%>
+  <%- attributes.select(&:token?).each do |attribute| -%>
+  has_secure_token<% if attribute.name != "token" %> :<%= attribute.name %><% end %>
+  <%- end -%>
+  <%- if attributes.any?(&:password_digest?) -%>
+  has_secure_password
+  <%- end -%>
 end
 <% end -%>
