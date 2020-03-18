ab1e6480adab8d5bb8cb15545144e47c8f943446
Martín Rosso <mrosso10@gmail.com>
2020-03-18 13:59:19 -0300

    bugfix de TimeWithZone#to_s

--- lib/pg_rails/core_ext.rb
+++ lib/pg_rails/core_ext.rb
@@ -1,6 +1,13 @@
 # require 'activesupport/time'
 class ActiveSupport::TimeWithZone
-  def to_s
-    strftime('%d/%m/%Y %H:%M')
+  def to_s(format = :default)
+    if format == :db
+      utc.to_s(format)
+    elsif formatter = ::Time::DATE_FORMATS[format]
+      formatter.respond_to?(:call) ? formatter.call(self).to_s : strftime(formatter)
+    else
+      # "#{time.strftime("%Y-%m-%d %H:%M:%S")} #{formatted_offset(false, 'UTC')}" # mimicking Ruby Time#to_s format
+      time.strftime('%d/%m/%Y %H:%M')
+    end
   end
 end
