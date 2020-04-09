81d2c86781ec924d4dab4c6c8d9ce27064fc1ccb
Martín Rosso <mrosso10@gmail.com>
2020-04-08 23:59:18 -0300

    arreglo de pg_form_for (no mergeaba bien las clases)

--- app/helpers/pg_rails/form_helper.rb
+++ app/helpers/pg_rails/form_helper.rb
@@ -2,8 +2,16 @@ module PgRails
   module FormHelper
     def pg_form_for(object, *args, &block)
       options = args.extract_options!
-      to_merge = { builder: PgFormBuilder, html: { class: 'pg-form'} }
-      simple_nested_form_for(object, *(args << options.merge(to_merge)), &block)
+
+      options[:builder] = PgFormBuilder
+      options[:html] ||= {}
+      if options[:html].key?(:class)
+        options[:html][:class] = ['pg-form', options[:html][:class]].compact
+      else
+        options[:html][:class] = 'pg-form'
+      end
+
+      simple_nested_form_for(object, *(args << options), &block)
     end
   end
 end
