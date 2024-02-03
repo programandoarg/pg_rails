feddd769fbf82b7538ef3ce8d78b805b2f0c0f4d
Martín Rosso <mrosso10@gmail.com>
2024-02-03 20:41:01 -0300

    toasts

--- spec/dummy/app/javascript/application.js
+++ spec/dummy/app/javascript/application.js
@@ -2,3 +2,35 @@
 import "@hotwired/turbo-rails"
 
 import * as bootstrap from 'bootstrap'
+
+
+// Bootstrap's toasts
+document.addEventListener('turbo:load', function () {
+  const toastElList = document.querySelectorAll('.toast:not(.hide):not(.show)')
+  Array.from(toastElList).map(toastEl => new bootstrap.Toast(toastEl).show())
+
+  // Select the node that will be observed for mutations
+  const targetNode = document.getElementById('flash')
+
+  // Options for the observer (which mutations to observe)
+  const config = { attributes: true, childList: true, subtree: true }
+
+  // Callback function to execute when mutations are observed
+  const callback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === 'childList') {
+        console.log('A child node has been added or removed.')
+        const toastElList = document.querySelectorAll('.toast:not(.hide):not(.show)')
+        Array.from(toastElList).map(toastEl => new bootstrap.Toast(toastEl).show())
+      } else if (mutation.type === 'attributes') {
+        console.log(`The ${mutation.attributeName} attribute was modified.`)
+      }
+    }
+  }
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback)
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config)
+})
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
       end
     end
   end
+  root "admin/personas#index"
   mount PgRails::Engine => "/pg_rails"
 end
