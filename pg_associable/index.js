d847026902606bb2d17ab74f999ef465233f4251
Martín Rosso <mrosso10@gmail.com>
2024-02-04 01:11:09 -0300

    asociable

---
+++ pg_associable/index.js
@@ -0,0 +1,7 @@
+import AsociableInlineController from './app/assets/js/asociable_inline_controller'
+import AsociableController from './app/assets/js/asociable_controller'
+import ModalController from './app/assets/js/modal_controller'
+
+window.Stimulus.register('asociable_inline', AsociableInlineController)
+window.Stimulus.register('asociable', AsociableController)
+window.Stimulus.register('modal', ModalController)
