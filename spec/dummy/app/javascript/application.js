cbd8892f0286edf32cfba316a4e4ba50127ffd3f
Martín Rosso <mrosso10@gmail.com>
2024-02-04 01:11:55 -0300

    asociable

--- spec/dummy/app/javascript/application.js
+++ spec/dummy/app/javascript/application.js
@@ -3,7 +3,7 @@ import "@hotwired/turbo-rails"
 import './controllers'
 import * as bootstrap from 'bootstrap'
 
-
+import './../../../../pg_associable'
 
 // Bootstrap's toasts
 document.addEventListener('turbo:load', function () {
