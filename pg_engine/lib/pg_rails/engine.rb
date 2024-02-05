d84da42d2169bea7b6472b0b07cdeea923f9eb5c
Martín Rosso <mrosso10@gmail.com>
2024-02-05 11:40:40 -0300

    Initializer

--- pg_engine/lib/pg_rails/engine.rb
+++ pg_engine/lib/pg_rails/engine.rb
@@ -8,12 +8,23 @@ module PgRails
     config.time_zone = 'America/Argentina/Buenos_Aires'
 
     initializer 'configurar_pg_rails' do
+      # SimpleForm
       require "#{PgRails::Engine.root}/../simple_form/simple_form"
       require "#{PgRails::Engine.root}/../simple_form/simple_form_bootstrap"
 
+      # Rainbow
       Rainbow.enabled = true
 
+      # Audited for ActiveAdmin
+      require 'audited/audit'
+      Audited::Audit.class_eval do
+        def self.ransackable_attributes(auth_object = nil)
+          columns.map(&:name)
+        end
+      end
+
       if Rails.env.development?
+        # Byebug
         require 'byebug/core'
         begin
           Byebug.start_server 'localhost', ENV.fetch('BYEBUG_SERVER_PORT', 8989).to_i
@@ -21,13 +32,13 @@ module PgRails
           Rails.logger.debug 'Byebug server already running'
         end
 
+        # Bullet
         Bullet.enable        = true
         Bullet.alert         = false
         Bullet.bullet_logger = true
         Bullet.console       = true
         Bullet.rails_logger  = true
         Bullet.add_footer    = true
-
       end
     end
   end
