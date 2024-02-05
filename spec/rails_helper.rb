25e1226c381c5d05dd725ae7c68eae1af79a3041
Martín Rosso <mrosso10@gmail.com>
2024-02-05 10:37:11 -0300

    specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start 'rails'
+
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 # require_relative '../config/environment'
@@ -7,6 +11,9 @@ require File.expand_path('dummy/config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+require 'capybara/rails'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
