382bff0b59bf5c585eab293b8c8d0ff6ccc46d34
Martín Rosso <mrosso10@gmail.com>
2024-02-05 10:37:37 -0300

    Byebug y bullet

--- pg_associable/lib/pg_associable/engine.rb
+++ pg_associable/lib/pg_associable/engine.rb
@@ -5,18 +5,8 @@ module PgAssociable
   class Engine < ::Rails::Engine
     isolate_namespace PgAssociable
     # initializer 'configurar_simple_form', after: 'factory_bot.set_fixture_replacement' do
-    initializer 'configurar_simple_form' do
+    initializer 'configurar_pg_scaffold' do
       require 'pg_associable/simple_form_initializer'
-
-      if Rails.env.development?
-        require 'byebug/core'
-        begin
-          Byebug.start_server 'localhost', ENV.fetch('BYEBUG_SERVER_PORT', 8989).to_i
-        rescue Errno::EADDRINUSE
-          Rails.logger.debug 'Byebug server already running'
-        end
-      end
-
     end
   end
 end
--- pg_engine/lib/pg_rails/engine.rb
+++ pg_engine/lib/pg_rails/engine.rb
@@ -7,9 +7,28 @@ module PgRails
     config.i18n.default_locale = :es
     config.time_zone = 'America/Argentina/Buenos_Aires'
 
-    initializer 'configurar_simple_form' do
+    initializer 'configurar_pg_rails' do
       require "#{PgRails::Engine.root}/../simple_form/simple_form"
       require "#{PgRails::Engine.root}/../simple_form/simple_form_bootstrap"
+
+      Rainbow.enabled = true
+
+      if Rails.env.development?
+        require 'byebug/core'
+        begin
+          Byebug.start_server 'localhost', ENV.fetch('BYEBUG_SERVER_PORT', 8989).to_i
+        rescue Errno::EADDRINUSE
+          Rails.logger.debug 'Byebug server already running'
+        end
+
+        Bullet.enable        = true
+        Bullet.alert         = false
+        Bullet.bullet_logger = true
+        Bullet.console       = true
+        Bullet.rails_logger  = true
+        Bullet.add_footer    = true
+
+      end
     end
   end
 end
