d1a8b5a7f135b0a84b68188868b2204ef4bdfb27
Martín Rosso <mrosso10@gmail.com>
2024-02-05 11:40:46 -0300

    activeadmin

--- spec/dummy/app/admin/audits.rb
+++ spec/dummy/app/admin/audits.rb
@@ -1,8 +1,3 @@
-Audited::Audit.class_eval do
-  def self.ransackable_attributes(auth_object = nil)
-    ["action", "associated_id", "associated_type", "auditable_id", "auditable_type", "audited_changes", "comment", "created_at", "id", "id_value", "remote_address", "request_uuid", "user_id", "user_type", "username", "version"]
-  end
-end
 ActiveAdmin.register Audited::Audit do
   # permit_params :discarded_at, :fecha, :nombre, :tiempo, :tipo, :created_at, :updated_at, :actualizado_por_id, :creado_por_id
 
@@ -29,8 +24,8 @@ ActiveAdmin.register Audited::Audit do
     actions
   end
 
-  # filter :fecha
-  # filter :nombre
+  filter :id
+  filter :nombre
   # filter :tiempo
   # filter :tipo
 
