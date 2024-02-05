957e40bf59740560276766463c4956a1186e0604
Martín Rosso <mrosso10@gmail.com>
2024-02-05 09:44:12 -0300

    cosas

--- spec/dummy/app/models/categoria_de_cosa.rb
+++ spec/dummy/app/models/categoria_de_cosa.rb
@@ -28,7 +28,7 @@
 # generado con pg_rails
 
 class CategoriaDeCosa < ApplicationRecord
-  audited
+  # audited
   include Discard::Model
 
   belongs_to :creado_por, optional: true, class_name: 'User'
@@ -38,5 +38,9 @@ class CategoriaDeCosa < ApplicationRecord
 
   scope :query, ->(param) { param.present? ? where(id: param) : all } # TODO: completar
 
-  validates :nombre, :tipo, presence: true
+  validates :nombre, :tipo, :tiempo, presence: true
+
+  validate do
+    errors.add(:base, 'cosas')
+  end
 end
