3d6caa8be3b21830a146b476139991a6ae71a243
Martín Rosso <mrosso10@gmail.com>
2024-02-05 10:45:40 -0300

    active admin install

--- Gemfile
+++ Gemfile
@@ -15,3 +15,5 @@ gemspec
 # gem 'byebug', group: [:development, :test]
 
 gem 'byebug'
+
+gem 'activeadmin'
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   namespace :admin do
     resources :categoria_de_cosas do
       collection do
