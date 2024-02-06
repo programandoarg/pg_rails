4619459ef23e279996e6629fe0a34819d4a173d6
Martín Rosso <mrosso10@gmail.com>
2024-02-06 16:04:45 -0300

    Borro cosas
    Borro cosas

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    PgEngine.resource_route(self, :categoria_de_cosas)
-  end
+    end
   devise_for :users
   ActiveAdmin.routes(self)
   root "admin/categoria_de_cosas#index"
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_175604) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_06_030038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -51,20 +51,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_175604) do
     t.index ["user_id", "user_type"], name: "user_index"
   end
 
-  create_table "categoria_de_cosas", force: :cascade do |t|
-    t.string "nombre", null: false
-    t.integer "tipo", null: false
-    t.date "fecha"
-    t.datetime "tiempo"
-    t.bigint "creado_por_id"
-    t.bigint "actualizado_por_id"
-    t.datetime "discarded_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["actualizado_por_id"], name: "index_categoria_de_cosas_on_actualizado_por_id"
-    t.index ["creado_por_id"], name: "index_categoria_de_cosas_on_creado_por_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -93,6 +79,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_175604) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
-  add_foreign_key "categoria_de_cosas", "users", column: "actualizado_por_id"
-  add_foreign_key "categoria_de_cosas", "users", column: "creado_por_id"
 end
