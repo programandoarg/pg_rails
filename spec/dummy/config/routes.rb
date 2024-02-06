25475588c32f4a7c2e57b9184368387f53039526
Martín Rosso <mrosso10@gmail.com>
2024-02-06 10:28:12 -0300

    Borro cosas
    borro cosas
    
    borro cosas

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,30 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   ActiveAdmin.routes(self)
   namespace :admin do
-    resources :blas do
-      collection do
-        get :abrir_modal
-        post :buscar
-      end
-    end
     resources :categoria_de_cosas do
       collection do
         get :abrir_modal
         post :buscar
       end
     end
-    resources :cosas do
-      collection do
-        get :abrir_modal
-        post :buscar
-      end
-    end
-    resources :personas do
-      collection do
-        get :abrir_modal
-        post :buscar
-      end
-    end
   end
-  root "admin/personas#index"
+  root "admin/categoria_de_cosas#index"
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -51,54 +51,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_030038) do
     t.index ["user_id", "user_type"], name: "user_index"
   end
 
-  create_table "blas", force: :cascade do |t|
-    t.string "nombre", null: false
-    t.integer "tipo", null: false
-    t.bigint "categoria_de_cosa_id", null: false
-    t.bigint "creado_por_id"
-    t.bigint "actualizado_por_id"
-    t.datetime "discarded_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["actualizado_por_id"], name: "index_blas_on_actualizado_por_id"
-    t.index ["categoria_de_cosa_id"], name: "index_blas_on_categoria_de_cosa_id"
-    t.index ["creado_por_id"], name: "index_blas_on_creado_por_id"
-  end
-
-  create_table "categoria_de_cosas", force: :cascade do |t|
-    t.string "nombre", null: false
-    t.integer "tipo", null: false
-    t.date "fecha"
-    t.datetime "tiempo"
-    t.bigint "creado_por_id"
-    t.bigint "actualizado_por_id"
-    t.datetime "discarded_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["actualizado_por_id"], name: "index_categoria_de_cosas_on_actualizado_por_id"
-    t.index ["creado_por_id"], name: "index_categoria_de_cosas_on_creado_por_id"
-  end
-
-  create_table "cosas", force: :cascade do |t|
-    t.string "nombre"
-    t.string "apellido"
-    t.bigint "persona_id"
-    t.bigint "creado_por_id"
-    t.bigint "actualizado_por_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["actualizado_por_id"], name: "index_cosas_on_actualizado_por_id"
-    t.index ["creado_por_id"], name: "index_cosas_on_creado_por_id"
-    t.index ["persona_id"], name: "index_cosas_on_persona_id"
-  end
-
-  create_table "personas", force: :cascade do |t|
-    t.string "nombre"
-    t.string "apellido"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -127,12 +79,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_030038) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
-  add_foreign_key "blas", "categoria_de_cosas"
-  add_foreign_key "blas", "users", column: "actualizado_por_id"
-  add_foreign_key "blas", "users", column: "creado_por_id"
-  add_foreign_key "categoria_de_cosas", "users", column: "actualizado_por_id"
-  add_foreign_key "categoria_de_cosas", "users", column: "creado_por_id"
-  add_foreign_key "cosas", "personas"
-  add_foreign_key "cosas", "users", column: "actualizado_por_id"
-  add_foreign_key "cosas", "users", column: "creado_por_id"
 end
