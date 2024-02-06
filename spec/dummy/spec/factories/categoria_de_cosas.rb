c8f2406f74103ed3e4e3dbdbeef67edf479c1b3b
Martín Rosso <mrosso10@gmail.com>
2024-02-06 15:01:21 -0300

    Categorias

--- spec/dummy/spec/factories/categoria_de_cosas.rb
+++ spec/dummy/spec/factories/categoria_de_cosas.rb
@@ -1,6 +1,30 @@
 # frozen_string_literal: true
 
-# generado con pg_rails
+# == Schema Information
+#
+# Table name: categoria_de_cosas
+#
+#  id                 :bigint           not null, primary key
+#  discarded_at       :datetime
+#  fecha              :date
+#  nombre             :string           not null
+#  tiempo             :datetime
+#  tipo               :integer          not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  actualizado_por_id :bigint
+#  creado_por_id      :bigint
+#
+# Indexes
+#
+#  index_categoria_de_cosas_on_actualizado_por_id  (actualizado_por_id)
+#  index_categoria_de_cosas_on_creado_por_id       (creado_por_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (actualizado_por_id => users.id)
+#  fk_rails_...  (creado_por_id => users.id)
+#
 
 FactoryBot.define do
   factory :categoria_de_cosa do
