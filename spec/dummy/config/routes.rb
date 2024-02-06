c662fb98cff7c4161b00d7c9abfc448174763a4d
Martín Rosso <mrosso10@gmail.com>
2024-02-06 14:55:02 -0300

    Borro cosas

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   ActiveAdmin.routes(self)
-  namespace :admin do
-    resources :categoria_de_cosas do
-      collection do
-        get :abrir_modal
-        post :buscar
-      end
-    end
-  end
   root "admin/categoria_de_cosas#index"
 end
