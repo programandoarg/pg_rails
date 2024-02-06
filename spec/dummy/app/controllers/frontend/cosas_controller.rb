34a5d765887ba9d191249cc4e71654c250122fb2
Martín Rosso <mrosso10@gmail.com>
2024-02-06 17:34:01 -0300

    cosas frontend

---
+++ spec/dummy/app/controllers/frontend/cosas_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# generado con pg_rails
+
+module Frontend
+  class CosasController < PgEngine::SignedInController
+    before_action { @clase_modelo = Cosa }
+
+    before_action(only: :index) { authorize Cosa }
+
+    before_action :set_instancia_modelo, only: %i[new create show edit update destroy]
+
+    add_breadcrumb Cosa.nombre_plural, :frontend_cosas_path
+
+    private
+
+    def atributos_permitidos
+      %i[nombre tipo categoria_de_cosa_id]
+    end
+
+    def atributos_para_buscar
+      %i[nombre tipo categoria_de_cosa]
+    end
+
+    def atributos_para_listar
+      %i[nombre tipo categoria_de_cosa]
+    end
+
+    def atributos_para_mostrar
+      %i[nombre tipo categoria_de_cosa]
+    end
+  end
+end
