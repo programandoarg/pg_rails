b27481084107d68c633e0f92837f5907cd7e4456
Martín Rosso <mrosso10@gmail.com>
2024-02-06 14:00:10 -0300

    Devise views
    Devise custom views

--- pg_layout/lib/pg_layout.rb
+++ pg_layout/lib/pg_layout.rb
@@ -1,5 +1,7 @@
-# Para que la viewpath de pg_layout tenga precedencia sobre la de kaminari
+# Para que la viewpath de pg_layout tenga precedencia sobre las de kaminari y devise
 require 'kaminari'
+require 'devise'
+require 'devise-i18n'
 
 require_relative 'pg_layout/engine'
 
