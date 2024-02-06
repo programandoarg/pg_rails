a0b09c1e5a686e66f0796b59dba72da3103fd9ec
Martín Rosso <mrosso10@gmail.com>
2024-02-06 16:38:04 -0300

    ActiveAdmin audits

--- pg_engine/lib/templates/activeadmin/audits.rb
+++ pg_engine/lib/templates/activeadmin/audits.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Audited::Audit do
-  # permit_params :discarded_at, :fecha, :nombre, :tiempo, :tipo, :created_at, :updated_at, :actualizado_por_id, :creado_por_id
+  permit_params [:auditable_id, :auditable_type, :associated_id, :associated_type, :user_id,
+                 :user_type, :username, :action, :audited_changes, :version, :comment,
+                 :remote_address, :request_uuid, :created_at]
 
   # :id
   # :discarded_at, :fecha, :nombre, :tiempo, :tipo, :created_at, :updated_at, :actualizado_por_id, :creado_por_id
@@ -13,21 +15,31 @@ ActiveAdmin.register Audited::Audit do
     # column :associated_type
     column :user_id
     # column :user_type
-    column :username
+    # column :username
     column :action
     # column :audited_changes
-    # column :version
-    # column :comment
-    # column :remote_address
+    column :version
+    column :comment
+    column :remote_address
     # column :request_uuid
     column :created_at
     actions
   end
 
-  filter :id
-  filter :nombre
-  # filter :tiempo
-  # filter :tipo
+  filter :auditable_id
+  filter :auditable_type
+  filter :associated_id
+  filter :associated_type
+  filter :user_id
+  filter :user_type
+  filter :username
+  filter :action
+  # filter :audited_changes
+  filter :version
+  filter :comment
+  filter :remote_address
+  filter :request_uuid
+  filter :created_at
 
   # form do |f|
   #   f.inputs do
