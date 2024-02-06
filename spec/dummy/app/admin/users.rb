9ea125086beda15e88c22218ab9493698d221caf
Martín Rosso <mrosso10@gmail.com>
2024-02-06 01:10:50 -0300

    ActiveAdmin

--- spec/dummy/app/admin/users.rb
+++ spec/dummy/app/admin/users.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  decorate_with UserDecorator
+  permit_params :email, :password, :password_confirmation, :confirmed_at, profiles: []
 
   index do
     selectable_column
@@ -15,10 +16,49 @@ ActiveAdmin.register User do
 
   form do |f|
     f.inputs do
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
+      if params[:change_password]
+        f.input :password
+        # f.input :password_confirmation
+      else
+        f.input :email
+        if !f.object.persisted?
+          f.input :password
+        end
+        f.input :profiles, as: :select
+      end
+      # f.input :password_confirmation
     end
     f.actions
+  end
+
+  before_update do |user|
+    user.skip_reconfirmation!
+  end
+  after_build do |user|
+    if action_name == 'create'
+      user.skip_confirmation!
+    end
+  end
+
+  member_action :discard, method: :put do
+    resource.discard!
+    redirect_to resource_path, notice: "Discarded!"
+  end
+
+  member_action :restore, method: :put do
+    resource.undiscard!
+    redirect_to resource_path, notice: "Restored!"
+  end
+
+  action_item :view, only: :show do
+    link_to 'Discard', discard_active_admin_user_path(resource), method: :put if resource.kept?
+  end
+
+  action_item :view, only: :show do
+    link_to 'Restore', restore_active_admin_user_path(resource), method: :put if resource.discarded?
+  end
+
+  action_item :view, only: :show do
+    link_to 'Cambiar contraseña', edit_active_admin_user_path(resource, change_password: true)
   end
 end
