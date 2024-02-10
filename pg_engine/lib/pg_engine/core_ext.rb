8d10298ea2fbe1e61da9de325c1a6aa652e7d2a6
Martín Rosso <mrosso10@gmail.com>
2024-02-10 19:03:20 -0300

    Rubofixes

--- pg_engine/lib/pg_engine/core_ext.rb
+++ pg_engine/lib/pg_engine/core_ext.rb
@@ -6,7 +6,7 @@
 #     def to_s(format = :default)
 #       if format == :db
 #         utc.to_s(format)
-#       elsif formatter = ::Time::DATE_FORMATS[format] # rubocop:disable Lint/AssignmentInCondition
+#       elsif formatter = ::Time::DATE_FORMATS[format]
 #         formatter.respond_to?(:call) ? formatter.call(self).to_s : strftime(formatter)
 #       else
 #         # "#{time.strftime("%Y-%m-%d %H:%M:%S")} #{formatted_offset(false, 'UTC')}" # mimicking Ruby Time#to_s format
