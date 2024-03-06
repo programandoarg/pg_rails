437efabfbe42bf81abbb06635960544aa47fb6b9
Martín Rosso <mrosso10@gmail.com>
2024-03-06 11:42:31 -0300

    Simpleform locale

--- pg_engine/config/simple_form/simple_form_bootstrap.rb
+++ pg_engine/config/simple_form/simple_form_bootstrap.rb
@@ -56,8 +56,8 @@ SimpleForm.setup do |config|
     b.optional :readonly
     b.use :label, class: 'form-label'
     b.use :input, class: 'form-control', error_class: 'is-invalid'
-    b.use :full_error, wrap_with: { class: 'invalid-feedback' }
-    b.optional :error, wrap_with: { class: 'invalid-feedback' }
+    # b.use :full_error, wrap_with: { class: 'invalid-feedback' }
+    b.use :error, wrap_with: { class: 'invalid-feedback' }
     b.use :hint, wrap_with: { class: 'form-text' }
   end
 
@@ -66,8 +66,8 @@ SimpleForm.setup do |config|
     b.optional :readonly
     b.use :label, class: 'form-label'
     b.use :input, class: 'form-select', error_class: 'is-invalid'
-    b.use :full_error, wrap_with: { class: 'invalid-feedback' }
-    b.optional :error, wrap_with: { class: 'invalid-feedback' }
+    # b.use :full_error, wrap_with: { class: 'invalid-feedback' }
+    b.use :error, wrap_with: { class: 'invalid-feedback' }
     b.use :hint, wrap_with: { class: 'form-text' }
   end
 
