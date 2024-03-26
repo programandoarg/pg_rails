cfa9237a2c27d16f5e5b4b000ca061536b8317c9
Martín Rosso <mrosso10@gmail.com>
2024-03-26 18:26:33 -0300

    Anycable

---
+++ spec/dummy/app/channels/bechmark_channel.rb
@@ -0,0 +1,24 @@
+class BenchmarkChannel < ActionCable::Channel::Base
+  def subscribed
+    stream_from "my_stream"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def broadcast(data)
+    # byebug
+    # puts "aslkdjask"
+    # AnyCable.broadcast("my_stream", { text: data['message'] })
+    ActionCable.server.broadcast("my_stream", data['message'])
+
+  end
+
+  def echo(data)
+    # byebug
+    # puts "aslkdjask"
+    AnyCable.broadcast("my_stream", 'ECHOOO')
+    # ActionCable.server.broadcast("some_channel", "ECHOOO")
+  end
+end
