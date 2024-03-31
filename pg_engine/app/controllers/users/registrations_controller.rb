03e2efddbc6a546f4a159ec661861c3c604a5f90
Martín Rosso <mrosso10@gmail.com>
2024-03-31 14:59:09 -0300

    Lint

--- pg_engine/app/controllers/users/registrations_controller.rb
+++ pg_engine/app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-  def after_inactive_sign_up_path_for(resource)
-    flash[:toast] = false
-    super(resource)
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    def after_inactive_sign_up_path_for(resource)
+      flash[:toast] = false
+      super(resource)
+    end
   end
 end
