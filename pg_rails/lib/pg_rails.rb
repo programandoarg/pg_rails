ad82fe2948a0dde7f70fb88373a407a153dfdc4b
Martín Rosso <mrosso10@gmail.com>
2024-04-08 22:53:15 -0300

    Dont load pg_scaffold on prod

--- pg_rails/lib/pg_rails.rb
+++ pg_rails/lib/pg_rails.rb
@@ -1,6 +1,6 @@
 require "#{__dir__}/../../pg_engine/lib/pg_engine"
 require "#{__dir__}/../../pg_associable/lib/pg_associable"
-require "#{__dir__}/../../pg_scaffold/lib/pg_scaffold"
+require "#{__dir__}/../../pg_scaffold/lib/pg_scaffold" if Rails.env.local?
 require "#{__dir__}/../../pg_layout/lib/pg_layout"
 
 module PgRails
