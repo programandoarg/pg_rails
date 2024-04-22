c92658fad7065f78c2332cc41187f446f460612d
Martín Rosso <mrosso10@gmail.com>
2024-04-22 12:35:55 -0300

    No muestro errores de presence en simple_form, pero mantengo los mensajes en active model

--- pg_engine/config/initializers/simple_form_monkey_patch.rb
+++ pg_engine/config/initializers/simple_form_monkey_patch.rb
@@ -2,6 +2,13 @@ module SimpleForm
   module Components
     module Errors
       def error_text
+        begin
+          if object.errors.details[attribute_name].map(&:values).flatten.include? :blank
+            return
+          end
+        rescue StandardError => e
+          pg_err e
+        end
         text = has_custom_error? ? options[:error] : errors.reject(&:empty?).send(error_method)
         return if text.blank?
 
