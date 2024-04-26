569ba4ad24d629c87b1c00cc8a525c7439cc2400
Martín Rosso <mrosso10@gmail.com>
2024-04-26 11:44:59 -0300

    Logo big

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < PgEngine::BaseController
     @navbar.logo = ApplicationController.render(
       partial: 'layouts/logo', locals: { image_url: 'logo-navbar-1.png', href: root_path }
     )
-    @navbar.logo_xl_url = 'facturabien.png'
+    @navbar.logo_xl_url = 'logo-xl-light.png'
     # @navbar.logo_xl = ApplicationController.render(
     #   partial: 'layouts/logo', locals: { image_url: 'facturabien.png', href: root_path }
     # )
