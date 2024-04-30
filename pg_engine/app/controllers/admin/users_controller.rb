bdd571feb4e5d20d8baaf9e659a6e4a11e201a2d
Martín Rosso <mrosso10@gmail.com>
2024-04-30 20:29:06 -0300

    [auto commit] users_controller.rb (Lint on prepush)

--- pg_engine/app/controllers/admin/users_controller.rb
+++ pg_engine/app/controllers/admin/users_controller.rb
@@ -32,14 +32,14 @@ module Admin
 
     # :nocov:
     def login_as
-      if dev_user_or_env?
-        usuario = User.find(params[:id])
-        if usuario.confirmed_at.present?
-          sign_in(:user, usuario)
-          redirect_to after_sign_in_path_for(usuario)
-        else
-          go_back('No está confirmado')
-        end
+      return unless dev_user_or_env?
+
+      usuario = User.find(params[:id])
+      if usuario.confirmed_at.present?
+        sign_in(:user, usuario)
+        redirect_to after_sign_in_path_for(usuario)
+      else
+        go_back('No está confirmado')
       end
     end
     # :nocov:
