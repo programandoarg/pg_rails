0704c387fa921f0c83ea286f1e94e6003bddca23
Martín Rosso <mrosso10@gmail.com>
2024-05-04 12:34:01 -0300

    Refactor

--- pg_engine/app/controllers/pg_engine/base_controller.rb
+++ pg_engine/app/controllers/pg_engine/base_controller.rb
@@ -31,22 +31,22 @@ module PgEngine
     def internal_error(error)
       pg_err error
 
+      @error_msg = <<~HTML.html_safe # rubocop:disable Rails/OutputSafety
+        <div>
+          Ocurrió algo inesperado
+          <br>
+          Por favor, intentá nuevamente
+          <br>
+          o <a class="text-decoration-underline" href="#{new_public_mensaje_contacto_path}">ponete en contacto</a> y pronto lo resolveremos
+        </div>
+      HTML
+
       respond_to do |format|
         format.html do
           render 'pg_layout/error', layout: 'pg_layout/containerized'
         end
         format.turbo_stream do
-          html = <<~HTML.html_safe # rubocop:disable Rails/OutputSafety
-            <div>
-              Ocurrió algo inesperado
-              <br>
-              Por favor, intentá nuevamente
-              <br>
-              o <a class="text-decoration-underline" href="#{new_public_mensaje_contacto_path}">ponete en contacto</a> y pronto lo resolveremos
-            </div>
-          HTML
-
-          flash.now[:critical] = html
+          flash.now[:critical] = @error_msg
           render turbo_stream: (turbo_stream.remove_all('.modal') + render_turbo_stream_flash_messages)
         end
       end
