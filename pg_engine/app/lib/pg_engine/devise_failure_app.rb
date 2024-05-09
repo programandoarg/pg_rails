1a3e4e117d3f4571185054b64e7e927d2e18ce89
Martín Rosso <mrosso10@gmail.com>
2024-05-09 13:19:03 -0300

    Si no está confirmado e intenta loguear, muestro el link de reenviar instrucciones

---
+++ pg_engine/app/lib/pg_engine/devise_failure_app.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+require 'devise'
+
+module PgEngine
+  # Checks the scope in the given environment and returns the associated failure app.
+  class DeviseFailureApp < Devise::FailureApp
+    # def self.call(env)
+    #   super(env)
+    # end
+
+    def respond
+      if warden_message == :unconfirmed
+        render_unconfirmed
+      else
+        super
+      end
+    end
+
+    private
+
+    def user_email
+      params[:user][:email]
+    rescue StandardError => e
+      pg_warn e, :warn
+    end
+
+    def render_unconfirmed
+      self.content_type = "text/vnd.turbo-stream.html"
+      self.status = 200
+      self.response_body = <<~HTML
+        <turbo-stream action="update" target="flash">
+          <template>
+            <div class="alert alert-warning d-flex align-items-center">
+              <div class="bi bi-exclamation-circle me-3"></div>
+              <span>
+                Tu cuenta aún no está confirmada.
+                <br>
+                Revisá tu bandeja de entrada o&nbsp;
+                <a href="/users/confirmation/new?email=#{user_email}">hacé click acá para reenviar el correo</a>
+              </span>
+            </div>
+          </template>
+        </turbo-stream>
+      HTML
+    end
+  end
+end
