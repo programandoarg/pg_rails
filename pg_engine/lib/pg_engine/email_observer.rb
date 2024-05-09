1746f419dcdaec23abbe45961e1bfbb815e50e87
Martín Rosso <mrosso10@gmail.com>
2024-05-09 13:41:22 -0300

    [auto commit] email_observer.rb (Some changes that wasn't commited on prepush)

--- pg_engine/lib/pg_engine/email_observer.rb
+++ pg_engine/lib/pg_engine/email_observer.rb
@@ -2,7 +2,7 @@
 
 module PgEngine
   class EmailObserver
-    def self.delivered_email(message)
+    def self.delivered_email(message) # rubocop:disable Metrics/AbcSize
       message_id = message.message_id
       mailer = message.delivery_handler.to_s
       status = get_status(message)
@@ -14,7 +14,8 @@ module PgEngine
         pg_warn 'El mail no tenía objeto Email asociado.', :warn
         to = [message.to].flatten.join(', ')
         from = [message.from].flatten.join(', ')
-        email = Email.create!(message_id:, subject: message.subject, to:, mailer:, status:, from_address: from, from_name: '')
+        email = Email.create!(message_id:, subject: message.subject, to:, mailer:, status:, from_address: from,
+                              from_name: '')
       end
       email.encoded_eml.attach({ io: StringIO.new(message.encoded), filename: "email-#{email.id}.eml" })
     rescue StandardError => e
