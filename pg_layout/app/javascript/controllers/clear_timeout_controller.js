f0649ecb2ba7d06c95a1227bb4e488dbc7d01129
Martín Rosso <mrosso10@gmail.com>
2024-05-13 12:57:26 -0300

    [auto commit] clear_timeout_controller.js index.js (Lint on prepush)

--- pg_layout/app/javascript/controllers/clear_timeout_controller.js
+++ pg_layout/app/javascript/controllers/clear_timeout_controller.js
@@ -1,9 +1,9 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="clear-timeout"
 export default class extends Controller {
-  connect() {
-    let timeoutId = parseInt(this.element.dataset.timeoutId)
+  connect () {
+    const timeoutId = parseInt(this.element.dataset.timeoutId)
     clearTimeout(timeoutId)
     console.log(`clearedTimeout: ${timeoutId}`)
     this.element.remove()
--- pg_layout/app/javascript/controllers/index.js
+++ pg_layout/app/javascript/controllers/index.js
@@ -4,10 +4,10 @@ import NavbarController from './navbar_controller'
 import NestedController from './nested_controller'
 import PgFormController from './pg_form_controller'
 import FadeinOnloadController from './fadein_onload_controller'
-import ClearTimeoutController from "./clear_timeout_controller"
+import ClearTimeoutController from './clear_timeout_controller'
 
 application.register('navbar', NavbarController)
 application.register('nested', NestedController)
 application.register('pg_form', PgFormController)
 application.register('fadein_onload', FadeinOnloadController)
-application.register("clear-timeout", ClearTimeoutController)
+application.register('clear-timeout', ClearTimeoutController)
