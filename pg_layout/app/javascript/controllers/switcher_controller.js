8c38aac2f1d64cf7d47c19189bac9636b196923a
Martín Rosso <mrosso10@gmail.com>
2024-05-19 08:06:44 -0300

    Switcher

---
+++ pg_layout/app/javascript/controllers/switcher_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="switcher"
+export default class extends Controller {
+  connect() {
+    if (this.element.checked) {
+      this.show()
+    }
+    this.element.setAttribute('data-action', `${this.element.getAttribute('data-action') || ''} switcher#show`)
+  }
+
+  show () {
+    let elemToShow = document.querySelector(this.element.dataset.target)
+    this.getSiblings(elemToShow).forEach((el) => {
+      el.classList.add('d-none')
+      el.classList.remove('d-block')
+    })
+    elemToShow.classList.remove('d-none')
+    elemToShow.classList.add('d-block')
+  }
+
+  getSiblings(el) {
+    const childrenArray = [ ...el.parentNode.children ];
+    return childrenArray.filter(child => child !== el);
+  }
+}
