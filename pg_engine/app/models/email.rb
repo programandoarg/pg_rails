51932d4e708e85beff274cc0b57e1c1e053f080b
Martín Rosso <mrosso10@gmail.com>
2024-05-22 17:41:56 -0300

    [autocommit] email.rb cosa_sin_email_object.html.slim

--- pg_engine/app/models/email.rb
+++ pg_engine/app/models/email.rb
@@ -36,6 +36,8 @@ class Email < ApplicationRecord
   include Hashid::Rails
   audited
 
+  attr_accessor :require_body_input
+
   has_one_attached :encoded_eml
 
   belongs_to :associated, polymorphic: true, optional: true
@@ -48,7 +50,7 @@ class Email < ApplicationRecord
   # TODO: y el fallido temporario?
   enumerize :status, in: { pending: 0, failed: 1, sent: 2, accepted: 3, delivered: 4, rejected: 5 }
 
-  validates :from_address, :to, :status, :body_input, presence: true
+  validates :from_address, :to, :status, presence: true
 
   validate do
     if to.present? && !to.split(/[,;]/).all? { |dest| dest.match(/\A[^@\s]+@[^@\s]+\z/) }
@@ -60,7 +62,9 @@ class Email < ApplicationRecord
   validates :subject, length: { within: 0..200 }
   validates :from_name, length: { within: 0..80 }
   validates :to, length: { within: 3..200 }
-  validates :body_input, length: { minimum: 10 }
+
+  validates :body_input, length: { minimum: 10 }, if: -> { require_body_input }
+  validates :body_input, presence: true, if: -> { require_body_input }
 
   validates :from_name, :subject, :to,
             format: { with: /\A[^\n<>&]*\z/, message: 'contiene caracteres inválidos' }
