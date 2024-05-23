143b711a50ec7c9f10e7bd85b88923263ef9199e
Martín Rosso <mrosso10@gmail.com>
2024-05-23 12:36:00 -0300

    MailLog spec

--- pg_engine/spec/lib/pg_engine/mailgun/log_sync_spec.rb
+++ pg_engine/spec/lib/pg_engine/mailgun/log_sync_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 require 'fileutils'
 
-describe PgEngine::Mailgun::LogSync, vcr: { cassette_name: 'mailgun/log_sync_download',
+fdescribe PgEngine::Mailgun::LogSync, vcr: { cassette_name: 'mailgun/log_sync_download',
                                             match_requests_on: %i[method host] } do
   let(:instancia) { described_class }
 
   before do
     FileUtils.rm_r(instancia.base_dir) if File.exists? instancia.base_dir
-  end
-  after do
-    # FileUtils.rm_r(instancia.base_dir)
   end
 
   describe '#download' do
@@ -20,24 +17,45 @@ describe PgEngine::Mailgun::LogSync, vcr: { cassette_name: 'mailgun/log_sync_dow
     let!(:email) { create :email, message_id: '66393f1bc7d4_47a5108ec1628f@notebook.mail' }
 
     it do
-      expect { subject }.to change { email.logs.to_a.length }.from(0).to(3)
+      expect { subject }.to (change { email.logs.count }.from(0).to(3))
+                                  .and(change(EmailLog, :count).to(99))
     end
   end
 
-  # describe '#digest_all' do
-  #   subject do
-  #     instancia.digest_all
-  #   end
+  describe '#digest' do
+    subject do
+      instancia.digest(log_data)
+    end
 
-  #   before do
-  #     from = PgEngine::Engine.root.join('spec/fixtures/mailgun_inbox')
-  #     to = instancia.inbox_dir
-  #     FileUtils.mkdir_p(instancia.base_dir)
-  #     FileUtils.cp_r from, to
-  #   end
+    let(:log_data) do
+      <<~JSON
+        {
+            "timestamp": 1715014542.2477064,
+            "ip": "66.102.8.333",
+            "event": "delivered",
+            "id": "log_2",
+            "severity":"temporary",
+            "log-level": "info",
+            "message": {
+                "headers": { "message-id": "msgid@fakeapp2024.mail" }
+            },
+            "recipient": "natprobel@fakemail.com"
+        }
+      JSON
+    end
 
-  #   fit do
-  #     expect { subject }.to change { Dir["#{instancia.inbox_dir}/*.json"].count }.to(0)
-  #   end
-  # end
+    it do
+      expect { subject }.to change(EmailLog, :count).by(1)
+    end
+
+    it do
+      expect(subject).to have_attributes(
+        id: 'log_2',
+        event: 'delivered',
+        log_level: 'info',
+        severity: 'temporary',
+        message_id: 'msgid@fakeapp2024.mail'
+      )
+    end
+  end
 end
