43a33879ce633979817b295daf10d2188f6f8c76
Martín Rosso <mrosso10@gmail.com>
2024-05-24 11:51:56 -0300

    Webhook fix

--- pg_engine/app/controllers/public/webhooks_controller.rb
+++ pg_engine/app/controllers/public/webhooks_controller.rb
@@ -1,10 +1,15 @@
 module Public
   class WebhooksController < PublicController
+    skip_before_action :verify_authenticity_token
+
     def mailgun
       # FIXME: verify signature
       # params['signature']['timestamp']
-      PgEngine::Mailgun::LogSync.digest(params['event-data'])
-      head :ok
+      if PgEngine::Mailgun::LogSync.digest(params['event-data'])
+        head :ok
+      else
+        head :not_acceptable
+      end
     end
   end
 end
