dbe98008be3abed4bd183536026d943ba800bef8
Martín Rosso <mrosso10@gmail.com>
2024-06-08 23:01:18 -0300

    HealthController

--- pg_engine/config/routes.rb
+++ pg_engine/config/routes.rb
@@ -1,6 +1,7 @@
 include PgEngine::RouteHelpers
 
 Rails.application.routes.draw do
+  get "pg_engine/health" => "pg_engine/health#show", as: :pg_engine_health_check
   namespace :public, path: '' do
     pg_resource(:mensaje_contactos, only: [:new, :create], path: 'contacto')
     post 'webhook/mailgun', to: 'webhooks#mailgun'
