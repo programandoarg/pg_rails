b953d5bc1f71755cf1c0ac9fbb23bf629dba27aa
Martín Rosso <mrosso10@gmail.com>
2024-06-10 22:40:19 -0300

    SimpleUserNotifier

---
+++ pg_engine/app/notifiers/simple_user_notifier.rb
@@ -0,0 +1,24 @@
+# To deliver this notification:
+#
+# SimpleUserNotifier.with(record: @post, message: "New post").deliver(User.all)
+
+class SimpleUserNotifier < ApplicationNotifier
+  # Add your delivery methods
+  #
+  # deliver_by :email do |config|
+  #   config.mailer = "UserMailer"
+  #   config.method = "new_post"
+  # end
+  #
+  # bulk_deliver_by :slack do |config|
+  #   config.url = -> { Rails.application.credentials.slack_webhook_url }
+  # end
+  #
+  # deliver_by :custom do |config|
+  #   config.class = "MyDeliveryMethod"
+  # end
+
+  # Add required params
+  #
+  required_param :message
+end
