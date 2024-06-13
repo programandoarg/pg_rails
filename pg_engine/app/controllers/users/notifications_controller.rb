7f5d4046ec7c008d7ec63b11811fd36582e5b736
Martín Rosso <mrosso10@gmail.com>
2024-06-13 11:31:51 -0300

    Style y comment

--- pg_engine/app/controllers/users/notifications_controller.rb
+++ pg_engine/app/controllers/users/notifications_controller.rb
@@ -1,7 +1,8 @@
 module Users
   class NotificationsController < ApplicationController
     def mark_as_seen
-      # FIXME: handle errors
+      # No handleo errores porque no debería fallar, y si falla
+      # se notifica a rollbar y al user no le pasa nada
       notifications = Noticed::Notification.where(id: params[:ids].split(','))
       notifications.each(&:mark_as_seen!)
       head :ok
