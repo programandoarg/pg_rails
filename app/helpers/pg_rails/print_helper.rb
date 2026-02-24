b84f7736f12d6c94d4b6a213d822d8e183e668de
Martín Rosso <mrosso10@gmail.com>
2026-02-24 09:47:06 -0300

    fixup! fix: print helper #print_value fk values

--- app/helpers/pg_rails/print_helper.rb
+++ app/helpers/pg_rails/print_helper.rb
@@ -135,16 +135,19 @@ module PgRails
           if nombre_clase.constantize.defined_enums[field].present?
             valor = nombre_clase.constantize.defined_enums[field].invert[value]
             I18n.t("enums.#{nombre_clase.downcase}.#{field}.#{valor}", default: valor)
-          elsif nombre_clase.constantize.respond_to?(:enumerized_attributes) && @clase_modelo.enumerized_attributes[field.to_s].present?
+          elsif nombre_clase.constantize.respond_to?(:enumerized_attributes) && nombre_clase.constantize.enumerized_attributes[field.to_s].present?
             nombre_clase.constantize.new(field => value).send("#{field}_text")
           else
             truncate_title(value.to_s.encode("UTF-8", invalid: :replace, undef: :replace))
           end
-        rescue NameError
+        rescue NameError => e
+          Rails.logger.error(e)
+          Rollbar.error(e)
           truncate_title(value.to_s.encode("UTF-8", invalid: :replace, undef: :replace))
         end
       end
     rescue StandardError => e
+      Rails.logger.error(e)
       Rollbar.error(e)
     end
 
